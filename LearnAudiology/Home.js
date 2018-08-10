366e4b53a35df845d3aba82a3cb488a8dfdb7c00
Gianni Poutanen <g.poutanen@uqconnec.edu.au>
2018-08-10 11:06:44 +1000

    added to home

--- LearnAudiology/Home.js
+++ LearnAudiology/Home.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
 
 export default class Home extends React.Component {
-  static navigationOptions = {
-    title: 'LearnAudiology',
-    headerTintColor: '#fff',
-    headerStyle: {
-      backgroundColor: 'rgb(94, 188, 241)',
-    },
-  }
   render() {
     const { navigate } = this.props.navigation;
     return ([
       <Text textAlign='center'>Press a Button</Text>,
       <Button onPress={() => navigate('Graph')} title='Audiogram'/>,
-      <Button onPress={() => navigate('Test')} title='Saved Graphs'/>,
+      <Button color="red" onPress={() => {console.log('Working')}, () => navigate('Test')} title='Saved Graphs'/>,
       <Button onPress={() => navigate('Test')} title='Test Yourself'/>,
       <Button onPress={() => navigate('Test')} title='Settings'/>,
+      <TouchableOpacity
+         style={{
+             borderWidth:0,
+             borderColor:'rgba(0,0,0,0.2)',
+             alignItems:'center',
+             justifyContent:'center',
+             alignSelf: 'center',
+             width: '90%',
+             height: 100,
+             backgroundColor:'rgb(94, 188, 241)',
+             borderRadius:50,
+     }}
+ ><Text>Audiograms</Text>
+ </TouchableOpacity>
     ]);
   }
 }
+
+const styles = {
+  buttonStyle: {
+    backgroundColor: "#ffaaa"
+  }
+};
